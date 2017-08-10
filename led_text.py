e16091667367e7842f37b174cd56baafda44a6f8
Sam Sobell <sam.sobell@gmail.com>
2017-08-09 18:57:08 -0700

    added basic files

---
+++ led_text.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import Image
+import ImageDraw
+import ImageFont
+import time
+from rgbmatrix import Adafruit_RGBmatrix
+
+def scroll_text(matrix, text):
+	font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSans.ttf", 31)
+	(width, height) = font.getsize(text)
+	txt = Image.new('RGBA', (width + 128, 32), (0, 0, 0, 0))
+	draw = ImageDraw.Draw(txt)
+
+	for n in range(width + 128):
+		draw.text((0, -4), text, fill = (128, 128, 128, 0), font = font)
+		matrix.SetImage(txt.im.id, 128 - n, 0)
+		time.sleep(0.01)
+
+	matrix.Clear()
+
+if __name__ == "__main__":
+	matrix = Adafruit_RGBmatrix(32, 4)
+	scroll_text(matrix, "Bush did 9/11 ;)")
---
+++ led_twitter.py
@@ -0,0 +1,21 @@
+import Image
+import twitter
+import led_text
+import time
+
+from rgbmatrix import Adafruit_RGBmatrix
+
+api = twitter.Api(consumer_key="srJbKZIU4J2Cy6xbDFVWO5oF9",
+                  consumer_secret="5RSXdVyrnOoJZIfZwySi77RtDFRQNWw6Uwlb7Pff2huYRVUqiM",
+                  access_token_key="57201894-S8UkCYPEuOlffiVASVIe5FKEhO3RsnS08wgXcuuzZ",
+                  access_token_secret="An6seL5SNVYTigN96XCiU67xy3YZeqv4w3GaKdpmJH55i")
+results = api.GetSearch(raw_query="q=from%3ArealDonaldTrump%20since%3A2017-08-08&src=typd")
+
+matrix = Adafruit_RGBmatrix(32, 4)
+saved_status = ""
+while(1):
+	statuses = api.GetUserTimeline(screen_name="realDonaldTrump")
+	if(saved_status != statuses[0].text):
+		saved_status = statuses[0].text
+		led_text.scroll_text(matrix, saved_status)
+	time.sleep(60)
