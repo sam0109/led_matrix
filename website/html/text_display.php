a9a8be53b31d1b095628a5cca87bdc4fa5cc19ba
Sam Sobell <sam.sobell@gmail.com>
2017-08-12 20:54:55 -0700

    bugfix

--- website/html/text_display.php
+++ website/html/text_display.php
@@ -1,5 +1,5 @@
 <?php
-	$command = escapeshellcmd('/home/sam/Documents/led_matrix/text_led.py');
+	$command = escapeshellcmd('sudo /home/sam/Documents/led_matrix/text_led.py');
 	$output = shell_exec($command);
 	echo $output;
 	//header( 'Location: /index.php' ) ;
